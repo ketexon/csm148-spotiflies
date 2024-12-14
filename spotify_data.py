ca50a72607bd6ea317a7a1854d4d8bb172497e3d
Aubrey Clark <zane.a.s.clark@gmail.com>
2024-12-14 13:42:26 -0800

    updated nn

--- spotify_data.py
+++ spotify_data.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
-data = pd.read_csv('csv_outputs/cleaned_spotify.csv')
-grouped_data = pd.read_csv('csv_outputs/grouped_cleaned_spotify.csv')
+data = pd.read_csv('spotify.csv')
+grouped_data = pd.read_csv('grouped_cleaned_spotify.csv')
 
 string_columns = ['track_id', 'artists', 'album_name', 'track_name']
 categorical_columns = ['key', 'mode', 'time_signature', 'track_genre']
